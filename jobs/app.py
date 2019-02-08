ea5ff506a69cb7ee54b97ef524ef8dfaa1c35d91
scottfreeman1996 <Scott61079@GB-LD1LPW10G008.mshome.net>
2019-02-08 09:32:31 +0000

    c

--- jobs/app.py
+++ jobs/app.py
@@ -17,7 +17,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
-        result = connection.commit()
+        results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
     
